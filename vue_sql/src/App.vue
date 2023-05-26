21c4ab9de35ac1d20a5511f76871d97197515b4d
QUGOR <93441911+Matatabe23@users.noreply.github.com>
2023-05-26 22:00:32 +0300

    1

--- vue_sql/src/App.vue
+++ vue_sql/src/App.vue
@@ -1,42 +1,63 @@
 <template>
-    <div>
-        <div class="main">
-            Вход
-            <input class="enter-main" placeholder="email">
-            <input class="enter-main" placeholder="password">
-        </div>
-    </div>
+	<div>
+		<div class="conteiner">
+			<div class="reg">
+				<h3>Вход</h3>
+				<input class="reg-down" v-model="email" type="text" placeholder="Email">
+				<input class="reg-down" v-model="password" type="text" placeholder="Password">
+				
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+import user from './user.js';
 
 export default {
-    data() {
-        return {
-            posts: [
-                {id: 1, name: 'Олег', email: 'vnsjd@mail.ru', password: 'dsfkmdfnviuern304u89hdhd'}
-            ],
+	data() {
+		return {
+			user: user,
+			email: '' ,
+			password: '',
 
-        };
-    },
-    async mounted() {
-        // const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
-        // this.posts = response.data;
-
-
-    },
+		};
+	},
+	async mounted() {
+		// const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+		// this.posts = response.data;
+	},
 };
 </script>
 
 <style>
-*{
-    margin: 0;
+* {
+	margin: 0;
+	background-color: #1f242d;
 }
-.main{
-    display: flex;
-    
-    justify-content: center;
+body{
+	background-color: #1f242d;
 }
-
-
+.reg{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+}
+.reg h3{
+	color: #0ef;
+	font-size: 45px;
+}
+.reg-down{
+	border: none;
+	border-radius: 20px;
+	margin-top: 15px;
+	padding: 15px 20px;
+	background-color: #0ef;
+	box-shadow: 0 0 10px #0ef;
+}
+.reg-down::placeholder{
+	color: black;
+}
 </style>
